57310188bc598de0e5f06ce2b66d0b7071c88b8b
sitrucp <sitrucp@users.noreply.github.com>
2022-01-03 21:10:02 -0500

    add new story

--- add_url.py
+++ add_url.py
@@ -8,26 +8,26 @@ print("Url is: " + new_url)
 # create new story object
 new_story = {
     "url": new_url,
-    "retrieval_status": "new",
-    "story_status": "open",
     "story_title": "",
     "story_date": "",
+    "comment_status": "open",
+    "html_to_csv": "no",
+    "retrieval_status": "pending",
     "comment_count_site": "",
     "comment_count_retrieved": "0",
-    "comment_retrieved_pct": "0",
-    "csv": "no"
+    "comment_retrieved_pct": "0"
 }
 
 # open json file
 file = os.path.dirname(os.path.abspath(__file__)) + '\cbc_stories.json'
 with open(file) as json_file:
     cbc_stories = json.load(json_file)
-    stories = cbc_stories['cbc_stories']
+    #stories = cbc_stories['cbc_stories']
     # append new story to list
-    stories.append(new_story)
+    cbc_stories.append(new_story)
     # create new file contents
-    new_data = {}
-    new_data['cbc_stories'] = stories
+    new_data = []
+    new_data = cbc_stories
 
 # write new file contents
 with open(file, 'w') as json_file:
